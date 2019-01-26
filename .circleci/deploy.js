b53bbd43ed5ffd261269aca50cc6d6167eeaf159
MaxAltena <Max@MaxAltena.com>
2019-01-26 14:15:21 +0100

    added circleci to upload to new.maxaltena.com

---
+++ .circleci/deploy.js
@@ -0,0 +1,17 @@
+var FtpDeploy = require("ftp-deploy");
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+  user: process.env.FTPUSERNAME,
+  password: process.env.FTPPASS,
+  host: process.env.FTPHOST,
+  port: 21,
+  localRoot: __dirname + "/../build",
+  remoteRoot: process.env.FTPPATH,
+  include: ["*"]
+};
+
+ftpDeploy.deploy(config, function(err) {
+  if (err) console.log(err);
+  else console.log("Uploaded to server");
+});
