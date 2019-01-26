c6fe7695302b8446ad86c9a295336866abdc327f
MaxAltena <Max@MaxAltena.com>
2019-01-26 02:06:18 +0100

    fixed some more screen issues

--- src/components/Footer/Goodbye.js
+++ src/components/Footer/Goodbye.js
@@ -5,18 +5,45 @@ const Goodbye = () => {
     <div className="Goodbye">
       <div className="row">
         <div
-          className="section col s10 offset-s2"
+          className="section col s12 m5 offset-m1"
           style={{ border: "2px solid black" }}
         >
-          def
+          <p>sitemap</p>
+          <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>skills</li>
+            <li>contact</li>
+            {/* space */}
+            <li>projects</li>
+            <ul>
+              <li>project1</li>
+              <li>project2</li>
+              <li>project3</li>
+            </ul>
+          </ul>
         </div>
-      </div>
-      <div className="row">
         <div
-          className="section col s10 offset-s2"
+          className="section col s6 m3"
           style={{ border: "2px solid black" }}
         >
-          abc
+          <p>social</p>
+          <ul>
+            <li>github</li>
+            <li>linkedin</li>
+            <li>flickr</li>
+            <li />
+          </ul>
+        </div>
+        <div
+          className="section col s6 m3"
+          style={{ border: "2px solid black" }}
+        >
+          <p>get in touch</p>
+          <ul>
+            <li>phone</li>
+            <li>email</li>
+          </ul>
         </div>
       </div>
     </div>
