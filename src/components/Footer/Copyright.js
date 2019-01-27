706d1f41f64269f125af7e7f2720b67da2113637
MaxAltena <Max@MaxAltena.com>
2019-01-27 14:28:24 +0100

    updated footer and its styling

--- src/components/Footer/Copyright.js
+++ src/components/Footer/Copyright.js
@@ -4,8 +4,26 @@ import Alink from "../common/Alink";
 const Copyright = () => {
   return (
     <div className="Copyright">
-      <div>&copy; {new Date().getFullYear()} Max Altena</div>
-      <div>
+      <div className="innerCopyright hide-on-med-and-down">
+        <div>
+          <p>&copy; {new Date().getFullYear()} Max Altena</p>
+        </div>
+        <div>
+          <Alink
+            type="Link"
+            to="/terms"
+            name="Terms & conditions"
+            classes="white-text"
+          />
+          <Alink
+            type="Link"
+            to="/credits"
+            name="Credits"
+            classes="white-text"
+          />
+        </div>
+      </div>
+      <div className="innerCopyright hide-on-large-only">
         <Alink
           type="Link"
           to="/terms"
@@ -13,6 +31,7 @@ const Copyright = () => {
           classes="white-text"
         />
         <Alink type="Link" to="/credits" name="Credits" classes="white-text" />
+        <p>&copy; {new Date().getFullYear()} Max Altena</p>
       </div>
     </div>
   );
--- src/components/Footer/Goodbye.js
+++ src/components/Footer/Goodbye.js
@@ -5,8 +5,8 @@ import Alink from "../common/Alink";
 const Goodbye = () => {
   return (
     <div className="Goodbye">
-      <div className="row">
-        <div className="section col m12 l6 hide-on-small-only">
+      <div className="row hide-on-small-only">
+        <div className="section col m12 l6">
           <p className="lowercase">Sitemap</p>
           <div className="row">
             <div className="col s6 m6 l4">
@@ -86,7 +86,111 @@ const Goodbye = () => {
             </div>
           </div>
         </div>
-        <div className="section col s6 offset-s3 hide-on-med-and-up">
+        <div className="section col m6 l3">
+          <p className="lowercase">Social</p>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/MaxAltena/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Alink grey-text text-lighten-2"
+              >
+                <i className="fab fa-github" />
+                <span className="inner">
+                  <span className="default">GitHub</span>
+                  <span className="hover">GitHub</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://codepen.io/MaxAltena/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Alink grey-text text-lighten-2"
+              >
+                <i className="fab fa-codepen" />
+                <span className="inner">
+                  <span className="default">CodePen</span>
+                  <span className="hover">CodePen</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://linkedin.com/in/MaxAltena/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Alink grey-text text-lighten-2"
+              >
+                <i className="fab fa-linkedin" />
+                <span className="inner">
+                  <span className="default">LinkedIn</span>
+                  <span className="hover">LinkedIn</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://flickr.com/people/MaxAltena/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Alink grey-text text-lighten-2"
+              >
+                <i className="fab fa-flickr" />
+                <span className="inner">
+                  <span className="default">Flickr</span>
+                  <span className="hover">Flickr</span>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="section col m6 l3">
+          <p className="lowercase">Get in touch</p>
+          <ul>
+            <li>
+              <a
+                href="tel: +31 6 39105742"
+                className="Alink grey-text text-lighten-2 lowercase"
+              >
+                <i className="material-icons">phone</i>
+                <span className="inner">
+                  <span className="default">+31 6 39105742</span>
+                  <span className="hover">+31 6 39105742</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:Max@MaxAltena.com"
+                className="Alink grey-text text-lighten-2 lowercase"
+              >
+                <i className="material-icons">email</i>
+                <span className="inner">
+                  <span className="default">Max@MaxAltena.com</span>
+                  <span className="hover">Max@MaxAltena.com</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="Alink grey-text text-lighten-2 lowercase"
+              >
+                <i className="material-icons">subject</i>
+                <span className="inner">
+                  <span className="default">Contact form</span>
+                  <span className="hover">Contact form</span>
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="row hide-on-med-and-up">
+        <div className="section col s12">
           <p className="lowercase">Sitemap</p>
           <div className="row">
             <div className="col s12">
@@ -162,7 +266,7 @@ const Goodbye = () => {
             </div>
           </div>
         </div>
-        <div className="section col s6 offset-s3 m6 l3">
+        <div className="section col s12">
           <p className="lowercase">Social</p>
           <ul>
             <li>
@@ -223,7 +327,7 @@ const Goodbye = () => {
             </li>
           </ul>
         </div>
-        <div className="section col s6 offset-s3 m6 l3">
+        <div className="section col s12">
           <p className="lowercase">Get in touch</p>
           <ul>
             <li>
