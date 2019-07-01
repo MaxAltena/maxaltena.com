86825661bf2eabd05f1fd0de46dd51fbc7d4d470
MaxAltena <Max@MaxAltena.com>
2019-07-01 20:44:09 +0200

    started working on projects

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import baffle from "baffle";
+import axios from "axios";
 
 export default class Projects extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      scrolledTo: false
+      scrolledTo: false,
+      loading: true,
+      projects: []
     };
   }
 
   componentDidMount() {
     this.scrollReveal();
     window.addEventListener("scroll", this.scrollReveal);
+
+    // axios
+    //   .get("https://api.github.com/users/MaxAltena/repos", {
+    //     params: {
+    //       type: "all",
+    //       sort: "updated"
+    //     }
+    //   })
+    //   .then(res => {
+    //     console.log(res);
+    //   });
   }
 
   componentWillUnmount() {
@@ -42,11 +56,14 @@ export default class Projects extends Component {
   };
 
   render() {
+    const { loading } = this.state;
+
     return (
       <div id="Projects">
         <a className="anchor" id="projects" />
         <h1 className="projects-baffle">Projects</h1>
         <p>Currently busy with adding projects [GitHub and Personal]...</p>
+        {loading ? <p>Loading</p> : <div>Projects</div>}
       </div>
     );
   }
