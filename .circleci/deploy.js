cefd4c8120dd53455d03ab7177dc76452d9ef6c4
Max Altena <Max@MaxAltena.com>
2020-02-23 21:28:10 +0100

    minor

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -2,17 +2,17 @@ var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-  user: process.env.FTPUSERNAME,
-  password: process.env.FTPPASSWORD,
-  host: process.env.FTPHOST,
-  port: 21,
-  localRoot: __dirname + "/../public",
-  remoteRoot: process.env.FTPPATH,
-  deleteRemote: true,
-  include: ["*", ".htaccess"]
+	user: process.env.FTPUSERNAME,
+	password: process.env.FTPPASSWORD,
+	host: process.env.FTPHOST,
+	port: 21,
+	localRoot: __dirname + "/../public",
+	remoteRoot: process.env.FTPPATH,
+	deleteRemote: true,
+	include: ["*", ".htaccess"]
 };
 
 ftpDeploy
-  .deploy(config)
-  .then(res => console.log("Uploaded to server.", res))
-  .catch(err => console.log(err));
+	.deploy(config)
+	.then(res => console.log("Uploaded to server.", res))
+	.catch(err => console.log(err));
