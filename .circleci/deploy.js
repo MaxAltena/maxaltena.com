42281ba6fe672c4805ef5d2bc7a698c20a83a2ae
Max Altena <Max@MaxAltena.com>
2020-03-17 09:51:31 +0100

    Updated circleci

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -1,18 +1,15 @@
 var FtpDeploy = require("ftp-deploy");
-var ftpDeploy = new FtpDeploy();
 
-var config = {
-	user: process.env.FTPUSERNAME,
-	password: process.env.FTPPASSWORD,
-	host: process.env.FTPHOST,
-	port: 21,
-	localRoot: __dirname + "/../public",
-	remoteRoot: process.env.FTPPATH,
-	deleteRemote: true,
-	include: ["*", ".htaccess"]
-};
-
-ftpDeploy
-	.deploy(config)
+new FtpDeploy()
+	.deploy({
+		user: process.env.FTPUSERNAME,
+		password: process.env.FTPPASSWORD,
+		host: process.env.FTPHOST,
+		port: 21,
+		localRoot: __dirname + "/../public",
+		remoteRoot: process.env.FTPPATH_BASE + process.env.FTPPATH,
+		deleteRemote: true,
+		include: ["*", ".htaccess"]
+	})
 	.then(res => console.log("Uploaded to server.", res))
-	.catch(err => console.log(err));
+	.catch(err => console.log("Failed to upload to server.", err));
