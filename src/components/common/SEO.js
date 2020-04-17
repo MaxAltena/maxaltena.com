d9f69b35a8b45b5251ac15fbfa7169d07b71d3d9
Max Altena <Max@MaxAltena.com>
2020-04-17 13:23:32 +0200

    small fixes

--- src/components/common/SEO.js
+++ src/components/common/SEO.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 
 export default class SEO extends Component {
   static propTypes = {
